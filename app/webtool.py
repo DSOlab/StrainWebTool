4591e947c855a8ff25a6b6bec84b983820a6da15
demanast@triangle <danast@mail.ntua.gr>
2018-12-20 16:21:45 +0200

    new templates, structure

--- app/webtool.py
+++ app/webtool.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug import secure_filename
 app = Flask(__name__, template_folder="templates")
 
-@app.route('/StrainTool')
+UPLOAD_FOLDER = '/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'vel'])
+
+@app.route('/StrainTool/website')
 def website():
   return  render_template('website/index.html')
 
-@app.route('/WebTool')
+@app.route('/StrainWebTool')
 def webtool():
   return render_template('webtool/index.html')
+
+@app.route('/StrainWebTool/inputs')
+def webtool_inputs():
+  return render_template('webtool/tmpl_inputs.html')
+
+@app.route('/StrainWebTool/parameters', methods=['GET', 'POST'])
+def webtool_params():
+  if request.method == 'POST':
+    file = request.files['file']
+    a = file.read()
+    result = a
+  return render_template('webtool/tmpl_params.html', content = result)
+
+
+@app.route('/StrainWebTool/results')
+def webtool_results():
+  return render_template('webtool/tmpl_results.html')
